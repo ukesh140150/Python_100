f22ee998277314d9d04d1302108dae14e7d3019f
ukesh140150 <ukesh140150@gmail.com>
2026-07-14 18:25:25 +0530

    password generator project added

---
+++ Password_Generator/main.py
@@ -0,0 +1,100 @@
+from tkinter import *
+from tkinter import messagebox
+import random
+import json
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+               'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+               'U','V','W','X','Y','Z']
+    numbers = ['0','1','2','3','4','5','6','7','8','9']
+    symbols = ['!','#','$','%','&','(',')','*','+']
+
+    password_letters = [random.choice(letters) for _ in range(random.randint(8,10))]
+    password_symbols = [random.choice(symbols) for _ in range(random.randint(2,4))]
+    password_numbers = [random.choice(numbers) for _ in range(random.randint(2,4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website=website_entry.get()
+    email=email_entry.get()
+    password=password_entry.get()
+    new_data={
+        website:{
+            "email":email,
+            "password":password,
+        }}
+
+    if len(website)==0 or len(password)==0:
+        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
+    else:
+         try:
+                with open("data.json", "r") as data_file:
+                    data=json.load(data_file)
+        
+         except FileNotFoundError:
+                with open("data.json", "w") as data_file:
+                    json.dump(new_data, data_file , indent=4)
+         else:
+            data.update(new_data)   
+            with open("data.json", "w") as data_file:
+                    json.dump(data, data_file , indent=4)   
+                    
+         finally: 
+            website_entry.delete(0, END)
+    password_entry.delete(0, END)
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Generator")
+window.config(padx=20, pady=20)
+
+canvas= Canvas(width=200, height=200)
+
+import os
+
+print("Current Working Directory:", os.getcwd())
+print("Files:", os.listdir())
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+logo_path = os.path.join(BASE_DIR, "logo.png")
+
+logo_img=PhotoImage(file=logo_path)
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
+
+# Labels
+website_label=Label(text="Website:")
+website_label.grid(row=1 , column=0)
+email_label=Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+password_label=Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+#entries
+website_entry=Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
+email_entry=Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "angela@gmail.com")
+password_entry=Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+#buttons
+generate_password_button=Button(text="Generate Password" , command=generate_password)
+generate_password_button.grid(row=3, column=2) 
+add_button=Button(text="Add", width=36 , command=save)
+add_button.grid(row=4, column=1, columnspan=2,)
+
+window.mainloop()
