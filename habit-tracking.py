4fb3d659eba5f663e33a9e0a8a2b12bbb51c8faf
ukesh140150 <ukesh140150@gmail.com>
2026-07-27 21:57:13 +0530

    habbit tracking added

--- habit-tracking.py
+++ habit-tracking.py
@@ -14,9 +14,6 @@ user_params = {
     "notMinor": "yes",
 }
 
-## POST
-# response = requests.post(url=pixela_endpoint, json=user_params)
-# print(response.text)
 
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
@@ -32,13 +29,10 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
-# print(response.text)
-
 pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
 
 today = datetime.now()
-# print(today.strftime("%Y%m%d"))
+
 
 pixel_data = {
     "date": today.strftime("%Y%m%d"),
@@ -54,14 +48,6 @@ new_pixel_data = {
     "quantity": "4.5"
 }
 
-## PUT
-# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
-# print(response.text)
-
-
 delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
 
 
-## DELETE
-# response = requests.delete(url=delete_endpoint, headers=headers)
-# print(response.text)
