1cfce10a5bb73d60b02c5a8284b6a9f30841a575
ukesh140150 <ukesh140150@gmail.com>
2026-07-28 23:56:04 +0530

    iss_part2

--- ISS_OverHead.py
+++ ISS_OverHead.py
@@ -3,21 +3,20 @@ from datetime import datetime
 import smtplib
 import time
 
-MY_EMAIL = "___YOUR_EMAIL_HERE____"
-MY_PASSWORD = "___YOUR_PASSWORD_HERE___"
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+MY_EMAIL = "ukesh140150@gmail.com"
+MY_PASSWORD = "Rockstar?"
+MY_LAT = 51.507351 
+MY_LONG = -0.127758 
 
 
 def is_iss_overhead():
-    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response = requests.get("http://api.open-notify.org/iss-now.json")
     response.raise_for_status()
     data = response.json()
 
     iss_latitude = float(data["iss_position"]["latitude"])
     iss_longitude = float(data["iss_position"]["longitude"])
 
-    #Your position is within +5 or -5 degrees of the iss position.
     if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
         return True
 
